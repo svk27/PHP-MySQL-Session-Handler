42d4e49e9edf4a6ddfa1c07bef5df851b136ff7c
Programster <sdpagent@gmail.com>
2018-01-14 17:33:38 +0000

    * Making the class final with private attributes. * Fixed an issue with the read()

--- src/SessionHandler.php
+++ src/SessionHandler.php
@@ -3,30 +3,33 @@
 namespace Programster\SessionHandler;
 
 
-class SessionHandler implements \SessionHandlerInterface
+final class SessionHandler implements \SessionHandlerInterface
 {
-    protected $dbConnection; # the mysqli connection
-    protected $dbTable; # name of the db table to store sessions in
+    private $dbConnection; # the mysqli connection
+    private $dbTable; # name of the db table to store sessions in
+    private $m_maxAge;
     
     
     /**
      * Create the session handler.
      * @param \mysqli $mysqli - the database connection to store sessions in.
      * @param string $tableName - the table within the database to store session data in.
+     * @param int $maxAge - the maximum age in seconds of a session variable.
      */
-    public function __construct(\mysqli $mysqli, string $tableName)
+    public function __construct(\mysqli $mysqli, string $tableName, int $maxAge=86400)
     {
         $this->dbConnection = $mysqli;
         $this->dbTable = $tableName;
+        $this->m_maxAge = $maxAge;
         
         
         $createSessionsTableQuery = 
             "CREATE TABLE IF NOT EXISTS `" . $this->dbTable . "` (
                 `id` varchar(32) NOT NULL,
-                `timestamp` int(10) unsigned DEFAULT NULL,
+                `modified_timestamp` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 `data` mediumtext,
                 PRIMARY KEY (`id`),
-                KEY `timestamp` (`timestamp`)
+                KEY `modified_timestamp` (`modified_timestamp`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
         
         $this->dbConnection->query($createSessionsTableQuery);
@@ -35,34 +38,47 @@ class SessionHandler implements \SessionHandlerInterface
     
     public function open($savePath, $sessionName)
     {
-        $limit = time() - (3600 * 24);
-        $sql = sprintf("DELETE FROM %s WHERE timestamp < %s", $this->dbTable, $limit);
+        $sql =
+            "DELETE FROM `" . $this->dbTable . "` " . 
+            "WHERE `modified_timestamp` < (NOW() - INTERVAL " . $this->m_maxAge . " SECOND)";
+        
         return $this->dbConnection->query($sql);
     }
-
-
+    
+    
     public function close()
     {
         return $this->dbConnection->close();
     }
-
-
+    
+    
     public function read($id)
     {
         $sql = 
-            "SELECT `data`" . 
-            " FROM `" . $this->dbTable . "`" . 
-            " WHERE `id` = '" . $id . "'";
+            "SELECT `data` " . 
+            "FROM `" . $this->dbTable . "` " . 
+            "WHERE `id` = '" . $id . "'";
         
         $result = $this->dbConnection->query($sql);
+        
+        if ($result === FALSE)
+        {
+            $result = "";
+        }
+        else
+        {
+            $row = $result->fetch_assoc();
+            $result = $row['data'];
+        }
+        
         return $result;
     }
     
     
     public function write($id, $data)
     {
-        $sql = "REPLACE INTO $this->dbTable (id, data, timestamp)" . 
-               " VALUES('" . $id . "', '" . $data . "', '" . time() . "')";
+        $sql = "REPLACE INTO `" . $this->dbTable . "` (id, data)" . 
+               " VALUES('" . $id . "', '" . $data . "')";
         
         return $this->dbConnection->query($sql);
     }
@@ -71,7 +87,7 @@ class SessionHandler implements \SessionHandlerInterface
     public function destroy($id)
     {
         $sql = "DELETE FROM `" . $this->dbTable . " WHERE `id` = '" . $id . "'";
-        return $this->dbConnection->query($sql, $params);
+        return $this->dbConnection->query($sql);
     }
     
     
