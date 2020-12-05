d3ce0920d3aa560a1437a99b434e19f6078e5a4d
= <=>
2020-12-05 16:40:14 +0000

    fixes an issue with a missing ` in the code, which caused destroy function to return false/fail. Also tweaks to formatting.

--- src/SessionHandler.php
+++ src/SessionHandler.php
@@ -8,8 +8,8 @@ final class SessionHandler implements \SessionHandlerInterface
     private $dbConnection; # the mysqli connection
     private $dbTable; # name of the db table to store sessions in
     private $m_maxAge;
-    
-    
+
+
     /**
      * Create the session handler.
      * @param \mysqli $mysqli - the database connection to store sessions in.
@@ -21,46 +21,38 @@ final class SessionHandler implements \SessionHandlerInterface
         $this->dbConnection = $mysqli;
         $this->dbTable = $tableName;
         $this->m_maxAge = $maxAge;
-        
-        
-        $createSessionsTableQuery = 
-            "CREATE TABLE IF NOT EXISTS `" . $this->dbTable . "` (
+
+        $createSessionsTableQuery =
+            "CREATE TABLE IF NOT EXISTS `{$this->dbTable}` (
                 `id` varchar(32) NOT NULL,
                 `modified_timestamp` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 `data` mediumtext,
                 PRIMARY KEY (`id`),
                 KEY `modified_timestamp` (`modified_timestamp`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
-        
+
         $this->dbConnection->query($createSessionsTableQuery);
     }
-    
-    
+
+
     public function open($savePath, $sessionName)
     {
-        $sql =
-            "DELETE FROM `" . $this->dbTable . "` " . 
-            "WHERE `modified_timestamp` < (NOW() - INTERVAL " . $this->m_maxAge . " SECOND)";
-        
+        $sql = "DELETE FROM `{$this->dbTable}` WHERE `modified_timestamp` < (NOW() - INTERVAL {$this->m_maxAge} SECOND)";
         return $this->dbConnection->query($sql);
     }
-    
-    
+
+
     public function close()
     {
         return $this->dbConnection->close();
     }
-    
-    
+
+
     public function read($id)
     {
-        $sql = 
-            "SELECT `data` " . 
-            "FROM `" . $this->dbTable . "` " . 
-            "WHERE `id` = '" . $id . "'";
-        
+        $sql = "SELECT `data` FROM `{$this->dbTable}` WHERE `id` = '{$id}'";
         $result = $this->dbConnection->query($sql);
-        
+
         if ($result === FALSE)
         {
             $result = "";
@@ -70,35 +62,29 @@ final class SessionHandler implements \SessionHandlerInterface
             $row = $result->fetch_assoc();
             $result = $row['data'];
         }
-        
+
         return $result;
     }
-    
-    
+
+
     public function write($id, $data)
     {
-        $sql = "REPLACE INTO `" . $this->dbTable . "` (id, data)" . 
-               " VALUES('" . $id . "', '" . $data . "')";
-        
+        $sql = "REPLACE INTO `{$this->dbTable}` (id, data) VALUES('{$id}', '{$data}')";
         return $this->dbConnection->query($sql);
     }
-    
-    
+
+
     public function destroy($id)
     {
-        $sql = "DELETE FROM `" . $this->dbTable . " WHERE `id` = '" . $id . "'";
+        $sql = "DELETE FROM `{$this->dbTable}` WHERE `id` = '{$id}'";
         return $this->dbConnection->query($sql);
     }
-    
-    
+
+
     public function gc($maxlifetime)
     {
-        $sql = sprintf(
-            "DELETE FROM %s WHERE `timestamp` < '%s'", 
-            $this->dbTable, 
-            time() - intval($maxlifetime)
-        );
-        
+        $minTime = time() - intval($maxlifetime);
+        $sql = "DELETE FROM `{$this->dbTable}` WHERE `timestamp` < '{$minTime}'";
         return $this->dbConnection->query($sql);
     }
 }
